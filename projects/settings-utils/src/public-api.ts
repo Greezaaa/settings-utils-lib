5f3de7a39e0a80854ef3307a2c05285557b62a06
Greezaaa <greezaaa@gmail.com>
2025-08-24 06:31:28 +0200

    feat: add notification component, service, model, and styles for user notifications

--- projects/settings-utils/src/public-api.ts
+++ projects/settings-utils/src/public-api.ts
@@ -8,3 +8,8 @@ export * from './lib/font-size/font-size.service';
 // Scroll spy 
 export * from './lib/scroll-spy/scroll-spy.service';
 export * from './lib/scroll-spy/scroll-spy.directive';
+
+// Notifications
+export * from './lib/notification/notification.model';
+export * from './lib/notification/notification.component';
+export * from './lib/notification/notification.service';
