b01316b2ca22b7bc33563fecf8a0827e8a878b1d
Greezaaa <greezaaa@gmail.com>
2025-08-26 21:05:51 +0200

    feat: add LICENSE file and update README with correct license link; bump version to 1.0.0

--- projects/su-labs/notifications/src/public-api.ts
+++ projects/su-labs/notifications/src/public-api.ts
@@ -2,4 +2,7 @@
  * Public API Surface of notifications
  */
 
-export * from './lib/notifications';
+export * from './lib/notification.component';
+export * from './lib/notification.service';
+export * from './lib/notification.service.spec';
+export * from './lib/notification.model';
